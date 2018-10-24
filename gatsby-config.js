524185bbd6dfe4c46408f29a948f13bcaab6ebf4
Myles Enriquez <enri.myles@gmail.com>
2018-10-24 15:56:38 -0700

    Add google fonts plugin.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  plugins : [`gatsby-plugin-styled-components`]
+  siteMetadata: {
+    title: `Super mES`
+  },
+  plugins : [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `source sans pro\:300, 700`
+        ]
+      }
+    }
+  ]
 }
