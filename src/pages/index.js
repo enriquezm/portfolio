a4deba97d8b0fdef929f59f207445062e45eb5d0
Myles Enriquez <enri.myles@gmail.com>
2018-10-24 16:44:17 -0700

    Import styled component layout.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,19 @@
 import React from "react"
 import Header from '../components/Header'
 
+import MainWrapper from '../layouts/MainWrapper'
+
 class Main extends React.Component {
   componentDidMount() {
-    document.body.style.backgroundColor = '#141516';
+    // document.body.style.backgroundColor = '#141516';
+    document.body.style.margin = 0;
   }
   render() {
     return (
-      <div>
+      <MainWrapper>
         <Header />
         <h1>Hello, World!</h1>
-      </div>
+      </MainWrapper>
     )
   }
 }
