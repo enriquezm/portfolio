341adccc6d99d4d523c12a8a3bf8106bc200f30b
Myles Enriquez <enri.myles@gmail.com>
2018-10-23 23:55:09 -0700

    Create header component.

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const HeaderWrapper = styled.header`
+  color: #fff;
+`
+
+export default () => <HeaderWrapper>The Header</HeaderWrapper>
