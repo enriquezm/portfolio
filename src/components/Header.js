39e2a5820e753db3ba3e35a444792decaf349620
Myles Enriquez <enri.myles@gmail.com>
2018-10-24 16:43:55 -0700

    Add styles to navbar.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,30 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'gatsby'
 
-const HeaderWrapper = styled.header`
-  color: #fff;
+const NavBar = styled.header`
+  color: #333;
+
+  display: flex;
+  justify-content: space-between;
+  background-color: goldenrod;
+`
+const Logo = styled.div`
+  font-size: 2em;
 `
 
-export default () => <HeaderWrapper>The Header</HeaderWrapper>
+const Header = () => {
+  return (
+    <NavBar>
+      <Logo>
+        <Link to="/">Super mES</Link>
+      </Logo>
+      <div>
+        <Link to="/about/">About</Link>
+      </div>
+      <div><a href="https://twitter.com/designthrottle">Twitter</a></div>
+    </NavBar>
+  )
+}
+
+export default Header;
