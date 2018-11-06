30f38558f0e79e1d979e3e8777e1c5b825fa23b3
Myles Enriquez <enri.myles@gmail.com>
2018-11-05 21:13:28 -0800

    Set up config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: `SUPER mES`,
-    description: `Hijacking the human's visual system with motion and web animation.`
+    title: `SUPER meS`,
+    description: `Hijacking the human's visual system with motion and web animation. Or at least trying to.`
   },
   plugins : [
     `gatsby-plugin-styled-components`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name:  `pages`,
+        path: `${__dirname}/src/pages`
+      }
+    },
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
