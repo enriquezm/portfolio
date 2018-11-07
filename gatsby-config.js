e2c2c7dfa66475319c66e9343ad4a6e3f4bb0168
Myles Enriquez <enri.myles@gmail.com>
2018-11-06 21:10:51 -0800

    Update styles.

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
-          `roboto\:300, 900`
+          `roboto condensed\:300, 700`
         ]
       }
     },
