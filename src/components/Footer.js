521a8cd18e7ff2d9ee561a7f8f2416cf605d0684
Myles Enriquez <enri.myles@gmail.com>
2018-11-11 20:20:24 -0800

    Add icons.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FiHeart as Heart } from 'react-icons/fi';
 
 const Wrapper = styled.footer`
   margin: 50px 0;
+`
+const HeartIcon = styled(Heart)`
+  color: #FB3640;
 `
 
 
 const Footer = () => {
   return (
     <Wrapper>
-      <p>Created by Myles with Gatsby and Love.</p>
+      <p>Created by Myles with <a href="https://www.gatsbyjs.org/">Gatsby</a> and <HeartIcon />.</p>
     </Wrapper>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { FiGithub as Github, FiCodepen as Codepen } from "react-icons/fi";
+
 const Wrapper = styled.div`
   margin: 50px 0;
 `
@@ -10,12 +12,27 @@ const H1 = styled.h1`
 const P = styled.p`
   margin-top: 10px;
 `
+const Links = styled.div`
+  & > a {
+    margin-right: 10px;
+  }
+`
 
 const Header = () => {
   return (
     <Wrapper>
+    <div>
       <H1>Myles Enriquez</H1>
       <P>UI Engineer</P>
+      <Links>
+        <a href="https://github.com/enriquezm">
+          <Github size={20} />
+        </a>
+        <a href="https://codepen.io/mylesenri/">
+          <Codepen size={20} />
+        </a>
+      </Links>
+    </div>
     </Wrapper>
   )
 }
