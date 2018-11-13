63f17b277d94f17e202e025f60b404ddbc0f937c
Myles Enriquez <enri.myles@gmail.com>
2018-11-13 09:34:05 -0800

    Import module.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
-/* eslint-disable */
 import React from 'react'
 import { Link } from 'gatsby'
 import styled from 'styled-components'
+import Layout from '../components/Layout'
 
 const About = () => {
   return (
