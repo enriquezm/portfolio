54973c9d22ea16ec3180d8ade6b576d8f4b5b285
Myles Enriquez <enri.myles@gmail.com>
2018-11-28 22:44:35 -0800

    Add blog page.

---
+++ src/pages/blog.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import styled from 'styled-components';
+import Layout from '../components/Layout';
+import { graphql, Link } from 'gatsby';
+
+const ListItem = styled.div`
+  margin: 20px 0;
+  transition: all 0.5s;
+  &:hover {
+    transition: all 0.5s;
+    background:#FCFCFC;
+  }
+  & > a > h2 {
+    margin: 0;
+  }
+  & > p {
+    font-family: 'Roboto', sans-serif;
+    font-size: 14px;
+    margin: 5px 0;
+  }
+`
+
+
+const BlogPage = ({ data }) => {
+  const { edges } = data.allMarkdownRemark;
+  return (
+    <Layout>
+      <main>
+        <ul>
+          {
+            edges.map(edge => {
+              const {frontmatter} = edge.node;
+              return (
+                <li key={frontmatter.path}>
+                  <ListItem>
+                    <Link to={frontmatter.path}>
+                      <h2>{frontmatter.title}</h2>
+                    </Link>
+                    <p>{frontmatter.date}</p>
+                  </ListItem>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </main>
+    </Layout>
+  )
+}
+
+export default BlogPage;
+
+export const query = graphql`
+  query {
+    allMarkdownRemark (
+      sort: {order: DESC, fields: [frontmatter___date]}
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            path
+            date
+          }
+        }
+      }
+    }
+  }
+`
