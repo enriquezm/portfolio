c9e0f5822706b2ac43e2f5159d40fe858f3e2ee6
Myles Enriquez <enri.myles@gmail.com>
2018-11-28 22:46:05 -0800

    Add content.

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,6 +7,10 @@ const About = () => {
   return (
     <Layout>
       <p>Man I hate writing about myself...</p>
+      <p>Let's see...</p>
+      <p>I'm a frontend developer focused on designing and building user interfaces.</p>
+      <p>I have a bunch of projects out there on the interwebs. You can find some stuff on <a href="https://codepen.io/mylesenri/">codepen</a> or <a href="https://codesandbox.io/u/enriquezm">codesandbox</a>. I mainly develop with React but have worked with other frameworks in the past.</p>
+      <p>Most of my inspiration comes from 80s hacker movies and don't even get me started on cyberpunk movies.</p>
     </Layout>
   )
 
