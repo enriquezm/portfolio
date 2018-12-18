f39738f182c6bcd535250c485809f78985bf0286
Myles Enriquez <enri.myles@gmail.com>
2018-12-17 23:07:05 -0800

    Create layout for landing page.

---
+++ src/components/LandingLayout.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+import '../global-styles.css';
+
+const LayoutWrapper = styled.article`
+  height: 100vh;  
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const Container = styled.div`
+  max-width: 600px;
+  padding: 10px;
+`
+
+
+
+const LandingLayout = ( {children} ) => {
+  return (
+    <LayoutWrapper>
+      <Container>
+        {children}
+      </Container>
+    </LayoutWrapper>
+  )
+}
+
+export default LandingLayout;
