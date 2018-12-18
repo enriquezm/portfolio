33c672ade42fa4ad91e3362acd8ef18fb5d62eb9
Myles Enriquez <enri.myles@gmail.com>
2018-12-17 23:07:26 -0800

    Clean up.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Layout from '../components/Layout';
+import LandingLayout from '../components/LandingLayout';
 import Anime from 'react-anime';
 import styled from 'styled-components';
 import { graphql, Link } from 'gatsby';
 import Status from '../components/Status';
-import { FiGithub as Github, FiUser as Linkedin } from "react-icons/fi";
+import { FiGithub as Github, FiFileText as File, FiLinkedin as LinkedIn } from "react-icons/fi";
 
 const H1 = styled.h1`
   margin-bottom: 30px;
@@ -14,116 +14,51 @@ const P = styled.p`
   margin: 0 0 20px 0;
 `;
 
-const ButtonContainer = styled.div`
-  /* border: 1px solid blue; */
-
+const LinkContainer = styled.div`
   display: flex;
-  justify-content: space-between;
-  margin: 20px 0 60px 0;
-`;
 
-const Button = styled.a`
-  /* border: 1px solid purple; */
-  width: 45%;
-  padding: 8px 15px;
-  text-align: center;
-  transition: all 0.5s;
-  text-decoration: none;
-  border: 1px solid #fff;
-  color: #fff;
-
-  &.primary {
-    &:hover {
-      color: #2aff82;
-      box-shadow: 10px 10px #2aff82;
-      border: 1px solid #2aff82;
-      transition: all 0.3s;
-    }
-    &:active {
-      box-shadow: 4px 4px #2aff82;
-    }
-  }
-  &.secondary {
-    &:hover {
-      color: #8E26FF;
-      box-shadow: 10px 10px #8E26FF;
-      border: 1px solid #8E26FF;
-      transition: all 0.3s;
-    }
-    &:active {
-      box-shadow: 4px 4px #5772ff;
-    }
-  }
-`;
-
-const PostList = styled.div`
-
-`;
-const Post = styled.li`
   a {
-    color: #FF255B;
-    transition: all 0.3s;
-    &:hover {
-      color: #ff668c;
-      transition: all 0.3s;
+    margin: 10px 20px 30px 0;
+    transition: all 0.5s;
+    text-decoration: none;
+    color: #fff;
+
+    &.primary {
+      &:hover {
+        color: #2aff82;
+        transition: all 0.3s;
+      }
+      &:active {
+        box-shadow: 4px 4px #2aff82;
+      }
     }
-  }
-`;
-
-const IndexPage = ({data}) => {
-  const { edges } = data.allMarkdownRemark;
-  return (
-    <Layout>
-      <Anime delay={(e, i) => i * 300} opacity={[0, 1]} translateY={'1em'}>
-       <H1>Myles Enriquez</H1>
-       <P>I design/build interfaces and connect them to data.</P>
-       <ButtonContainer>
-         <Button className="primary" href="https://github.com/enriquezm"><Github /> Github</Button>
-         <Button className="secondary" href="https://www.linkedin.com/in/mylesenriquez/"><Linkedin /> LinkedIn</Button>
-       </ButtonContainer>
-       <Status />
-      </Anime>
-        <PostList>
-        <Anime delay={(e, i) => i * 300} opacity={[0, 1]}>
-          <ul>
-            {
-              edges.map(edge => {
-                const {frontmatter} = edge.node;
-                return (
-                  <Post key={frontmatter.path}>
-                    <div>
-                      <Link to={frontmatter.path}>
-                        <h2>{frontmatter.title}</h2>
-                      </Link>
-                      <p>{frontmatter.date}</p>
-                    </div>
-                  </Post>
-                )
-              })
-            }
-          </ul>
-          </Anime>
-        </PostList>
-    </Layout>
-  );
-}
-
-export default IndexPage;
-
-export const query = graphql`
-  query {
-    allMarkdownRemark (
-      sort: {order: DESC, fields: [frontmatter___date]}
-    ) {
-      edges {
-        node {
-          frontmatter {
-            title
-            path
-            date
-          }
-        }
+    &.secondary {
+      &:hover {
+        color: #8E26FF;
+        transition: all 0.3s;
+      }
+      &:active {
+        box-shadow: 4px 4px #5772ff;
       }
     }
   }
 `;
+
+const IndexPage = () => {
+  return (
+    <LandingLayout>
+      <Anime delay={(e, i) => i * 300} opacity={[0, 1]} translateY={'1em'}>
+       <H1>Myles Enriquez</H1>
+       <P>I design/build interfaces and connect them to data.</P>
+       <LinkContainer>
+        <Link className="primary" to="/blog/"><File /> Projects</Link> 
+         <a className="secondary" href="https://github.com/enriquezm"><Github /> Github</a>
+         <a className="secondary" href="https://www.linkedin.com/in/mylesenriquez/"><LinkedIn /> LinkedIn</a>
+       </LinkContainer>
+       <Status />
+      </Anime>
+    </LandingLayout>
+  );
+}
+
+export default IndexPage;
