f8d563a21d9c02a4f093e0d9b6ffd26bc54d290f
Myles Enriquez <enri.myles@gmail.com>
2018-12-18 23:32:38 -0800

    Fix icons.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,50 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'gatsby';
 
-import { FiGithub as Github, FiUser as Linkedin } from "react-icons/fi";
+import { FiHome as Home, FiGithub as Github, FiLinkedin as LinkedIn } from "react-icons/fi";
 
+const H1 = styled.h1`
+  margin-bottom: 10px;
+`;
+
+const P = styled.p`
+  margin: 0 0 20px 0;
+`;
+
+const LinkContainer = styled.div`
+margin-bottom: 50px;
+a {
+  margin-right: 20px;
+  transition: all 0.5s;
+  text-decoration: none;
+
+  &.primary {
+    color: #131212;
+    &:hover {
+      color: #8E26FF;
+      transition: all 0.3s;
+    }
+  }
+  &.secondary {
+    color: #131212;
+    &:hover {
+      color: #FF255B;
+      transition: all 0.3s;
+    }
+  }
+}
+`;
 
 const Header = () => {
   return (
       <div>
-        <h1>Myles Enriquez</h1>
-        <p>I make user interfaces.</p>
-        <div>
-          <Link to="/">Home</Link>
-        </div>
+        <H1>Myles Enriquez</H1>
+        <P>I design/build interfaces and connect them to data.</P>
+        <LinkContainer>
+          <Link className="primary" to="/"><Home size={20} /></Link>
+          <a className="secondary" href="https://github.com/enriquezm"><Github size={20} /></a>
+          <a className="secondary" href="https://www.linkedin.com/in/mylesenriquez/"><LinkedIn size={20} /></a>
+        </LinkContainer>
       </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,36 +12,28 @@ const H1 = styled.h1`
 
 const P = styled.p`
   margin: 0 0 20px 0;
+  color: #65636f;
 `;
 
 const LinkContainer = styled.div`
   display: flex;
-
+  margin-bottom: 50px;
   a {
-    margin: 10px 20px 30px 0;
-    padding: 10px 15px;
+    margin-right: 20px;
     transition: all 0.5s;
     text-decoration: none;
-    color: #131212;
-    border: 1px solid #131212;
-    border-radius: 4px;
-
+  
     &.primary {
-      border: 1px solid #8E26FF;
-      color: #8E26FF;
+      color: #131212;
       &:hover {
-        color: white;
-        background: #8E26FF;
-        border: 1px solid #8E26FF;
+        color: #8E26FF;
         transition: all 0.3s;
       }
     }
     &.secondary {
-      color: #FF255B;
-      border: 1px solid #FF255B;
+      color: #131212;
       &:hover {
-        color: white;
-        background-color: #FF255B;
+        color: #FF255B;
         transition: all 0.3s;
       }
     }
@@ -55,9 +47,9 @@ const IndexPage = () => {
        <H1>Myles Enriquez</H1>
        <P>I design/build interfaces and connect them to data.</P>
        <LinkContainer>
-        <Link className="primary" to="/blog/"><File /> Projects</Link> 
-         <a className="secondary" href="https://github.com/enriquezm"><Github /> Github</a>
-         <a className="secondary" href="https://www.linkedin.com/in/mylesenriquez/"><LinkedIn /> LinkedIn</a>
+        <Link className="primary" to="/blog/"><File size={20} /></Link> 
+        <a className="secondary" href="https://github.com/enriquezm"><Github size={20} /></a>
+        <a className="secondary" href="https://www.linkedin.com/in/mylesenriquez/"><LinkedIn size={20} /></a>
        </LinkContainer>
        <Status />
       </Anime>
