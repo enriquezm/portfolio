55c762573151851409ef2a88b910212b8069b8c2
Myles Enriquez <enri.myles@gmail.com>
2018-12-19 23:31:19 -0800

    Add plugins for in post image handling.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,19 @@ module.exports = {
   },
   plugins : [
     `gatsby-plugin-styled-components`,
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+        ]
+      }
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
