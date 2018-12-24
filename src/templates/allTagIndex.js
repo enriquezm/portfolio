79ad0572cc0c6549a662d34de1e5c99c13c069f6
Myles Enriquez <enri.myles@gmail.com>
2018-12-24 12:45:47 -0800

    Create all tag template.

---
+++ src/templates/allTagIndex.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import React from 'react';
+import {graphql, Link} from 'gatsby';
+
+const AllTagsTemplate = () => {
+    return (
+        <div>
+            <div>tags here</div>
+        </div>
+    );
+};
+
+export default AllTagsTemplate;
+
