b77668573c62851fed95673204e76d0cfd37d46a
Myles Enriquez <enri.myles@gmail.com>
2019-01-26 14:41:08 -0800

    Change layout.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,88 +1,55 @@
 import React from 'react';
-import LandingLayout from '../components/LandingLayout';
-import Anime from 'react-anime';
-import styled, {keyframes} from 'styled-components';
-import { Link } from 'gatsby';
-import Status from '../components/Status';
-import { FiGithub as Github, FiFileText as File, FiLinkedin as LinkedIn } from "react-icons/fi";
-
-const H1 = styled.h1`
-  margin-bottom: 30px;
-`;
-
-const P = styled.p`
-  margin: 0 0 20px 0;
-  color: #65636f;
-`;
-
-const buzz = keyframes`
-  0% {
-    transform: skewX(0);
-  }
-  10% {
-    transform: skewX(70deg);
-  }
-  15% {
-    transform: skewX(-70deg);
-  }
-  25% {
-    transform: skewX(25deg);
-  }
-  50% {
-    transform: skewX(-25deg);
-  }
-  75% {
-    transform: skewX(10deg);
-  }
-  90% {
-    transform: skewX(-10deg);
-  }
-  100% {
-    transform: skewX(0);
-  }
-`;
-
-const LinkContainer = styled.div`
-  display: flex;
-  margin-bottom: 50px;
-  a {
-    margin-right: 20px;
-    transition: all 0.5s;
-    text-decoration: none;
-  
-    &.primary {
-      color: #131212;
-      &:hover {
-        color: #8E26FF;
-        transition: all 0.3s;
-        animation: ${buzz} 0.3s;
-      }
-    }
-    &.secondary {
-      color: #131212;
-      &:hover {
-        color: #FF255B;
-        transition: all 0.3s;
-        animation: ${buzz} 0.3s;
-      }
-    }
-  }
-`;
 
 const IndexPage = () => {
   return (
-    <LandingLayout>
-      <Anime delay={(e, i) => i * 300} opacity={[0, 1]} translateY={'1em'}>
-       <H1>Myles Enriquez</H1>
-       <P>I design/build interfaces and connect them to data.</P>
-       <LinkContainer>
-        <Link className="primary" to="/blog/" title="Blog posts."><File size={20} /></Link> 
-        <a className="secondary" href="https://github.com/enriquezm" title="Github"><Github size={20} /></a>
-        <a className="secondary" href="https://www.linkedin.com/in/mylesenriquez/" title="LinkedIn"><LinkedIn size={20} /></a>
-       </LinkContainer>
-       <Status />
-      </Anime>
-    </LandingLayout>
+    <div>
+      <header class="flex-container flex-split margin-bottom-lg">
+        <div>
+          <div class="flex-content">
+            <a href="#"><h1>Myles Enriquez</h1></a>
+            <p class="font-color-violet">Frontend Engineer</p>
+            <div>
+              <a class="link-btn" href="#">resume</a>
+              <a class="link-btn" href="#">github</a>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div class="flex-content">
+            <p class="font-override">v1.1</p>
+          </div>
+        </div>
+      </header>
+      <main class="flex-container">
+      <section class="flex-column">
+        <div class="flex-content">
+            <h2 class="margin-bottom-xs all-caps">Projects</h2>
+            <p class="margin-bottom-sm font-color-light-gray-super">Some projects worth talking about. Check out my walkthrough, the source code, or the live/demo site.</p>
+          <ul class="margin-bottom-md">
+            <li class="margin-bottom-xs"><a class="margin-right-xs" href="#">habit rabbit</a><a class="link-btn margin-right-xs" href="#">source</a><a class="link-btn margin-right-xs" href="#">demo</a></li>
+            <li class="margin-bottom-xs"><a class="margin-right-xs" href="#">auralworth.com</a><a class="link-btn margin-right-xs" href="#">live site</a></li>
+            <li class="margin-bottom-xs"><a class="margin-right-xs" href="#">kellyintegrations.com</a><a class="link-btn margin-right-xs">live site</a></li>
+          </ul>
+            <h2 class="margin-bottom-xs all-caps">Concepts</h2>
+            <p class="margin-bottom-sm font-color-light-gray-super">Aren't quite full on projects, but shouldn't be thrown away...yet.</p>
+            <ul class="margin-bottom-md">
+            <li class="margin-bottom-xs"><a href="#">habit rabbit</a></li>
+            <li class="margin-bottom-xs"><a href="#">meos</a></li>
+            <li class="margin-bottom-xs"><a href="#">git balloons</a></li>
+          </ul>
+          <h2 class="margin-bottom-xs all-caps">Socialize</h2>
+          <ul class="margin-bottom-md">
+            <li class="margin-bottom-xs"><a href="#">github</a></li>
+            <li class="margin-bottom-xs"><a href="#">enri.myles@gmail.com</a></li>
+          </ul>
+        </div>
+      </section>
+
+    </main>
+      <footer>
+        <p class="font-override font-color-light-gray-super">Slapped together by <a href="#">Myles Enriquez</a>.</p>
+      </footer>
+    </div>
   );
 }
 
