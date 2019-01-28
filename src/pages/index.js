073bea37c2afe5c818e66c0e88c9bd463f65367b
Myles Enriquez <enri.myles@gmail.com>
2019-01-27 20:51:12 -0800

    Add query for posts.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,56 +1,75 @@
+/* eslint-disable */
 import React from 'react';
 
 const IndexPage = () => {
   return (
     <div>
-      <header class="flex-container flex-split margin-bottom-lg">
+      <header className="flex-container flex-split margin-bottom-lg">
         <div>
-          <div class="flex-content">
+          <div className="flex-content">
             <a href="#"><h1>Myles Enriquez</h1></a>
-            <p class="font-color-violet">Frontend Engineer</p>
+            <p className="font-color-violet">Frontend Engineer</p>
             <div>
-              <a class="link-btn" href="#">resume</a>
-              <a class="link-btn" href="#">github</a>
+              <a className="link-btn" href="#">resume</a>
+              <a className="link-btn" href="#">github</a>
             </div>
           </div>
         </div>
         <div>
-          <div class="flex-content">
-            <p class="font-override">v1.1</p>
+          <div className="flex-content">
+            <p className="font-override">v1.1</p>
           </div>
         </div>
       </header>
-      <main class="flex-container">
-      <section class="flex-column">
-        <div class="flex-content">
-            <h2 class="margin-bottom-xs all-caps">Projects</h2>
-            <p class="margin-bottom-sm font-color-light-gray-super">Some projects worth talking about. Check out my walkthrough, the source code, or the live/demo site.</p>
-          <ul class="margin-bottom-md">
-            <li class="margin-bottom-xs"><a class="margin-right-xs" href="#">habit rabbit</a><a class="link-btn margin-right-xs" href="#">source</a><a class="link-btn margin-right-xs" href="#">demo</a></li>
-            <li class="margin-bottom-xs"><a class="margin-right-xs" href="#">auralworth.com</a><a class="link-btn margin-right-xs" href="#">live site</a></li>
-            <li class="margin-bottom-xs"><a class="margin-right-xs" href="#">kellyintegrations.com</a><a class="link-btn margin-right-xs">live site</a></li>
+      <main className="flex-container">
+      <section className="flex-column">
+        <div className="flex-content">
+            <h2 className="margin-bottom-xs all-caps">Projects</h2>
+            <p className="margin-bottom-sm font-color-light-gray-super">Some projects worth talking about. Check out my walkthrough, the source code, or the live/demo site.</p>
+          <ul className="margin-bottom-md">
+            <li className="margin-bottom-xs"><a className="margin-right-xs" href="#">habit rabbit</a><a className="link-btn margin-right-xs" href="#">source</a><a className="link-btn margin-right-xs" href="#">demo</a></li>
+            <li className="margin-bottom-xs"><a className="margin-right-xs" href="#">auralworth.com</a><a className="link-btn margin-right-xs" href="#">live site</a></li>
+            <li className="margin-bottom-xs"><a className="margin-right-xs" href="#">kellyintegrations.com</a><a className="link-btn margin-right-xs">live site</a></li>
           </ul>
-            <h2 class="margin-bottom-xs all-caps">Concepts</h2>
-            <p class="margin-bottom-sm font-color-light-gray-super">Aren't quite full on projects, but shouldn't be thrown away...yet.</p>
-            <ul class="margin-bottom-md">
-            <li class="margin-bottom-xs"><a href="#">habit rabbit</a></li>
-            <li class="margin-bottom-xs"><a href="#">meos</a></li>
-            <li class="margin-bottom-xs"><a href="#">git balloons</a></li>
+            <h2 className="margin-bottom-xs all-caps">Concepts</h2>
+            <p className="margin-bottom-sm font-color-light-gray-super">Aren't quite full on projects, but shouldn't be thrown away...yet.</p>
+            <ul className="margin-bottom-md">
+            <li className="margin-bottom-xs"><a href="#">habit rabbit</a></li>
+            <li className="margin-bottom-xs"><a href="#">meos</a></li>
+            <li className="margin-bottom-xs"><a href="#">git balloons</a></li>
           </ul>
-          <h2 class="margin-bottom-xs all-caps">Socialize</h2>
-          <ul class="margin-bottom-md">
-            <li class="margin-bottom-xs"><a href="#">github</a></li>
-            <li class="margin-bottom-xs"><a href="#">enri.myles@gmail.com</a></li>
+          <h2 className="margin-bottom-xs all-caps">Socialize</h2>
+          <ul className="margin-bottom-md">
+            <li className="margin-bottom-xs"><a href="#">github</a></li>
+            <li className="margin-bottom-xs"><a href="#">enri.myles@gmail.com</a></li>
           </ul>
         </div>
       </section>
 
     </main>
       <footer>
-        <p class="font-override font-color-light-gray-super">Slapped together by <a href="#">Myles Enriquez</a>.</p>
+        <p className="font-override font-color-light-gray-super">Slapped together by <a href="#">Myles Enriquez</a>.</p>
       </footer>
     </div>
   );
 }
 
 export default IndexPage;
+
+export const query = graphql`
+  query {
+    allMarkdownRemark (
+      sort: {order: DESC, fields: [frontmatter___date]}
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            path
+            date
+          }
+        }
+      }
+    }
+  }
+`
