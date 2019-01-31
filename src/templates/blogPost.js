ff33950af81a257b0990a79c7f6dd8699bcb32d9
Myles Enriquez <enri.myles@gmail.com>
2019-01-30 20:25:05 -0800

    Clean up.

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,46 +1,8 @@
 import React from 'react';
 import { graphql, Link } from 'gatsby';
 import Layout from '../components/Layout';
-import styled from 'styled-components';
 import '../global-styles.css';
 
-
-const PostContainer = styled.div`
-  line-height: 1.5;
-  h2 {
-    margin-bottom: 5px;
-  }
-  pre {
-    padding: 10px;
-    border-radius: 3px;
-    background-color: #F2F2F4;
-    color: #4A4A4A;
-    font-size: 14px;
-  }
-  strong {
-    color: #7F00FF;
-  }
-`
-const Meta = styled.div`
-  display: flex;
-  margin-bottom: 20px;
-`;
-const Tags = styled.div`
-  display: flex;
-  div {
-    margin: 0 2.5px 0 2.5px;
-    padding: 1px 5px;
-    border-radius: 4px;
-    font-size: 12px;
-    background: #8E26FF;
-    color: white;
-  }
-`;
-const LinkContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
 
 const Template = ({data, pageContext}) => {
   
@@ -50,21 +12,34 @@ const Template = ({data, pageContext}) => {
   const title = markdownRemark.frontmatter.title;
   const html = markdownRemark.html;
   return (
-    <Layout>
-      <PostContainer>
-        <h2>{title}</h2>
-        <Meta>
-          <Tags>
-            Involves:
-            <div>React</div>
-            <div>MySQL</div>
-            <div>Nodejs</div>
-          </Tags>
-        </Meta>
-        <div dangerouslySetInnerHTML={{__html: html}} />
-      </PostContainer>
-      <hr />
-      <LinkContainer>
+    <div>
+      <header className="flex-container flex-split margin-bottom-lg">
+        <div>
+          <div className="flex-content">
+            <Link to="/"><h1>Myles Enriquez</h1></Link>
+            <p className="font-color-violet">Frontend Engineer</p>
+            <div>
+              <a className="link-btn" href="#">resume</a>
+              <a className="link-btn" href="https://github.com/enriquezm">github</a>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="flex-content">
+            <p className="font-override">v1.1</p>
+          </div>
+        </div>
+      </header>
+
+      <main className="flex-container">
+        <section className="flex-column">
+          <div className="flex-content">
+            <h2>{title}</h2>
+            <div dangerouslySetInnerHTML={{__html: html}} />
+          </div>
+        </section>
+      </main>
+
       {prev && 
           <Link to={prev.frontmatter.path}>
             Previous Post
@@ -75,8 +50,7 @@ const Template = ({data, pageContext}) => {
           Next Post
         </Link>
       }
-      </LinkContainer>
-    </Layout>
+    </div>
   )
 }
 
